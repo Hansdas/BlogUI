e0c366d649da7ae16f91e5e39cc4a311ae921e81
Mr.Han <3248996258@qq.com>
2019-09-20 17:41:22 +0800

    详情

--- style/js/request.js
+++ style/js/request.js
@@ -1,18 +1,26 @@
-function requestajax(route, methodtype, {datatype='json', async='false',func=''}) {
+
+function requestajax(args) {
+    var options={
+        route:args.route,
+        type:args.type||'get',
+        datatype:args.datatype||'json',
+        async:args.async|| false,
+        func:args.func||undefined,
+    };
     var result=undefined;
     $.ajax({
-        url: 'https://127.0.0.1:5001/blogh/' + route,
-        type: methodtype,
-        datatype: datatype,
-        async: async,
+        url: 'https://127.0.0.1:5001/blogh/' + options.route,
+        type: options.type,
+        datatype: options.datatype,
+        async: options.async,
         success: function (response) {
-            if(func=='')
+            if(typeof(options.func)==undefined)
             {
                 result=response;
             }
             else
             {
-                func(response);
+                options.func(response);
             }           
         },
         error: function (response) {
@@ -20,10 +28,6 @@ function requestajax(route, methodtype, {datatype='json', async='false',func=''}
         }
     });
     return result;
-}
-function invoke(func)
-{
-
 }
 function upload(route, methodtype, datatype, async) {
     var result;
