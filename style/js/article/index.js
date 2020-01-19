d38ba2c30251eb85415735b92070c070b0b0c841
Mr.Han <3248996258@qq.com>
2020-01-19 11:14:36 +0800

    点赞功能

--- style/js/article/index.js
+++ style/js/article/index.js
@@ -1,26 +1,121 @@
 
 layui.config({
     base: '/style/js/'
-}).use(['element', 'laypage', 'jquery', 'laytpl','layer'], function () {
+}).use(['element', 'laypage', 'jquery', 'laytpl', 'layer'], function () {
     element = layui.element, laypage = layui.laypage, $ = layui.$, laytpl = layui.laytpl, layer = layui.layer;
     var articletype;
     inittab();
     var listHtml = document.getElementById('article-item').innerHTML;
     laypage.render({
         elem: 'page'
-        ,limit: 10
+        , limit: 10
         , jump: function (obj) {
             var loading = layer.load(2);
             var pageSize = obj.limit;
             var pageIndex = obj.curr;
-            loadarticle(pageIndex, pageSize, listHtml, articletype,loading);
+            loadarticle(pageIndex, pageSize, listHtml, articletype, loading);
         },
-    });      
+    });
 })
+function praise(id) {
+    var loading = layer.load(2);
+    var text = $('#' + id).parent().find('span')[0].innerText;
+    var regex = /[^0-9]+/g;
+    var token=localStorage.getItem('token'); 
+    if (token ==null) {
+            layer.msg('你还未登录', {
+                time: 1500
+                ,icon: 7
+                ,offset: ['200PX', '450PX']
+            });
+            layer.close(loading);
+            return;
+    }
+    $.ajax({
+        url: url + 'article/praise/' + id,
+        type: 'post',
+        datatype: 'json',
+        beforeSend: function (xhr) {
+            doBeforeSend(xhr);
+        },
+        success: function (response) {
+            if (response.code == '0') {
+                layer.close(loading);
+                if (response.message == '0') {
+                    layer.msg("点赞成功", { icon: 6 ,offset: ['200PX', '450PX'] });
+                    var praiseCount = parseInt(text.replace(regex, '')) + 1;
+                    $('#' + id).parent().find('span')[0].innerText = '点赞(' + praiseCount + ')';
+                }
+                else if(message.message=='not login')
+                {
+                    layer.msg("请先登录", { icon: 7 ,offset: ['200PX', '450PX'] }); 
+                }
+                else {
+                    layer.open({
+                        title: '提示'
+                        , content: response.message
+                        , btn: ['确定', '取消']
+                        ,offset: ['200PX', '450PX']
+                        , yes: function () {
+                            $.ajax({
+                                url: url + 'article/praiseOut/' + id,
+                                type: 'post',
+                                datatype: 'json',
+                                beforeSend: function (xhr) {
+                                    doBeforeSend(xhr);
+                                },
+                                success: function (response) {
+                                    if (response.code == '0') {
+                                        layer.msg("取消点赞成功", { icon: 6,offset: ['200PX', '450PX']  });
+                                        var praiseCount = parseInt(text.replace(regex, '')) - 1;
+                                        $('#' + id).parent().find('span')[0].innerText = '点赞(' + praiseCount + ')';
+                                    }
+                                    else {
+                                        layer.msg('响应服务器失败', {
+                                            icon: 7
+                                        });
+                                    }
+                                },
+                                complete: function (xhr) {
+                                    doComplete(xhr);
+                                },
+                                error: function () {
+                                    layer.msg('响应服务器失败', {
+                                        icon: 7
+                                    });
+                                }
+
+                            });
+                        }
+                        , btn2: function (index) {
+                            layer.close(index);
+                        }
+
+                    });
+                }
+            }
+            else {
+                layer.msg('响应服务器失败', {
+                    icon: 7
+                });
+            }
+            layer.close(loading);
+
+        },
+        complete: function (xhr) {
+            doComplete(xhr);
+        },
+        error: function () {
+            layer.msg('响应服务器失败', {
+                icon: 7
+            });
+        }
+    });
+}
 /**
  * 加载
  */
-function loadarticle(pageIndex,pageSize,listHtml,articletype,loading) {
+function loadarticle(pageIndex, pageSize, listHtml, articletype, loading) {
     $.ajax({
         url: url + 'article/loadarticle',
         type: 'get',
@@ -34,33 +129,31 @@ function loadarticle(pageIndex,pageSize,listHtml,articletype,loading) {
             doBeforeSend(xhr);
         },
         success: function (response) {
-            if (response.code=='0') {
+            if (response.code == '0') {
                 var data = {
                     'list': response.data
                 };
                 articleview = document.getElementById('article-item-id');
                 laytpl(listHtml).render(data, function (html) {
                     articleview.innerHTML = html;
-                })               
-            } 
-            else
-            {
+                })
+            }
+            else {
                 layer.msg('响应服务器失败', {
                     icon: 7
                 });
             }
-            layer.close(loading);  
-          
+            layer.close(loading);
+
         },
         complete: function (xhr) {
             doComplete(xhr);
         },
-        error:function()
-        {
+        error: function () {
             layer.msg('响应服务器失败', {
                 icon: 7
-            }); 
-            layer.close(loading);    
+            });
+            layer.close(loading);
         }
     })
 }
@@ -84,6 +177,6 @@ function inittab() {
         } else {
             articletype = '';
         }
-        loadarticle(1, 10, articleDom, articletype,loading);
+        loadarticle(1, 10, articleDom, articletype, loading);
     });
 }
