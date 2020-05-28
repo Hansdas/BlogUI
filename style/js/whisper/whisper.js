4bd1f8f0cd944fbfa60f4a9397e219880a7bb11c
Mr.Han <3248996258@qq.com>
2020-05-28 17:38:05 +0800

    微语页面

--- style/js/whisper/whisper.js
+++ style/js/whisper/whisper.js
@@ -1,56 +1,15 @@
 ﻿var loading, laytpl, form;
 layui.config({
 	base: '/style/js/'
-}).use(['flow', 'form', 'laytpl', 'layedit'], function () {
-	var layedit = layui.layedit, flow = layui.flow;
+}).use(['flow', 'form', 'laytpl', 'layedit','laydate'], function () {
+	var layedit = layui.layedit, flow = layui.flow,laydate = layui.laydate;;
 	form = layui.form;
-	var textcontent = layedit.build('L_content', {
-		height: 140 //设置编辑器高度\
-		, tool: [
-			, 'underline' //下划线
-			, 'del' //删除线			
-			, '|' //分割线			
-			, 'left' //左对齐
-			, 'center' //居中对齐
-			, 'right' //右对齐
-			, 'link' //超链接
-			, 'unlink' //清除链接
-			, 'face' //表情
-		]
-	});
-	form.verify({
-		content: function () {
-			var content = layedit.getContent(textcontent);
-			if (content == "") {
-				return '请填写内容';
-			}
-		}
-	});
-	form.on('submit(save)', function (data) {
-		$.ajax({
-			url: url + 'whisper/publish',
-			type: 'post',
-			datatype: 'json',
-			data: {
-				'content': layedit.getContent(textcontent)
-			},
-			beforeSend: function (xhr) {
-				doBeforeSend(xhr);
-			},
-			success: function (response) {
-				if (response.code == "0") {
-					layer.msg("保存成功", { icon: 6 });
-				}
-				else {
-					layer.msg("响应服务器失败", { icon: 7 });
-				}
-			},
-			complete: function (xhr) {
-				doComplete(xhr);
-			},
-		});
-		return false;
-	});
+     var ins1=laydate.render({
+		elem: '#calendar'
+		,position: 'static'
+		  ,btns: ['now']
+		  ,calendar: true
+	  });
 	flow.load({
 		elem: '#time-axis' //流加载容器。
 		,end:'没有更多了' 
