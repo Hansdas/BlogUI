ebd7a36a2b7981d322db8c9bb98be4b9fe87d9ec
Mr.Han <3248996258@qq.com>
2020-07-09 15:22:25 +0800

    修改富文本编辑器

---
+++ style/ckeditor/config.js
@@ -0,0 +1,57 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbar = [
+        ['Source'], ['Save', 'NewPage', 'Preview', 'Print'], ['Templates'],
+        ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'], ['Undo', 'Redo'],
+        ['Find', 'Replace'], ['SelectAll'], ['Scayt'],
+        ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        //'/',
+        ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'], ['CopyFormatting', 'RemoveFormat'],
+        ['NumberedList', 'BulletedList'], ['Outdent', 'Indent'], ['Blockquote', 'CreateDiv'], ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'], ['BidiLtr', 'BidiRtl', 'Language'],
+        ['Link', 'Unlink', 'Anchor'],
+        ['Image'/*, 'Flash'*/, 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'],
+        //'/',
+        ['Styles', 'Format', 'Font', 'FontSize'], ['TextColor', 'BGColor'],
+        ['Maximize', 'ShowBlocks'],
+        ['About'],
+        //添加插件按钮
+        ['CodeSnippet', 'Markdown', 'uploadfile', 'diagram'/*, 'Diagramwidget'*/]
+    ];
+	config.removeButtons = 'Save,NewPage,Templates,Scayt,Language,Smiley,Styles,Format,About';
+	config.extraPlugins = "codesnippet";
+  
+
+    //设置文件、图片、Flash上传处理地址
+    //config.filebrowserBrowseUrl = "";     启用浏览服务器
+    config.filebrowserImageBrowseUrl ="http://127.0.0.1:5000/api/upload/image";
+    config.filebrowserImageUploadUrl ="http://127.0.0.1:5000/api/upload/image";
+    config.language = 'zh-cn';
+
+    /*去掉图片预览框的文字*/
+	config.image_previewText = ' ';
+	//移除图片上传页面的'高级','链接'页签
+	config.removeDialogTabs = 'image:advanced;image:Link';
+    //设置字体，需将config.js另存为utf-8编码格式，否则字体名乱码
+    config.font_names = '宋体/宋体, 新宋体;仿宋/仿宋_GB2312, 仿宋;黑体;楷体/楷体_GB2312, 楷体;隶书;微软雅黑;幼圆';
+
+    //设置回车格式
+    config.enterMode = CKEDITOR.ENTER_BR;
+
+    //禁用ACF
+    config.allowedContent = true;
+
+    //插入/编辑超链接时target默认设置为_blank
+    CKEDITOR.on('dialogDefinition', function (ev) {
+        var dialogName = ev.data.name;
+        var dialogDefinition = ev.data.definition;
+        if (dialogName == 'link') {
+            var targetTab = dialogDefinition.getContents('target');
+            var targetField = targetTab.get('linkTargetType');
+            targetField['default'] = '_blank';
+        }
+    });
+};
--- style/js/article/add.js
+++ style/js/article/add.js
@@ -1,9 +1,9 @@
 var loading, form,layedit,textcontent;
-layui.use(['form', 'layer', 'layedit'], function () {
+layui.use(['form', 'layer'], function () {
     var  $ = layui.jquery, layer = layui.layer;
     var filePaths = [];
+    editor= CKEDITOR.replace('ckEditor', { height: '400px'});
     form = layui.form;
-    layedit = layui.layedit;
     var id = getSearchString('id');
     bindselect();
     if (id == undefined) {
@@ -13,86 +13,7 @@ layui.use(['form', 'layer', 'layedit'], function () {
     else {
         bindArticle(id);
     }
-    layedit.set({
-        uploadImage: {
-            headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-            url: url + 'upload/image',
-            accept: 'image',
-            acceptMime: 'image/*',
-            exts: 'jpg|png|gif|bmp|jpeg',
-            size: 1024 * 10,
-            data: {
-                width: 1200,
-                height: 1200,
-                isAbs: false
-            },
-            done: function (res) {
-                if (res.code == 0) {
-                    filePaths.push(res.data.src);
-                }
-            }
-        },
-        // uploadVideo: {
-        //     url: '',
-        //     accept: 'video',
-        //     acceptMime: 'video/*',
-        //     exts: 'mp4|flv|avi|rm|rmvb',
-        //     size: 1024 * 10 * 2,
-        //     done: function (data) {
-        //         console.log(data);
-        //     }
-        // },
-        // uploadFiles: {
-        //     url: '',
-        //     accept: 'file',
-        //     acceptMime: 'file/*',
-        //     size: '20480',
-        //     autoInsert: true, //自动插入编辑器设置
-        //     done: function (data) {
-        //         console.log(data);
-        //     }
-        // }
-        //右键删除图片/视频时的回调参数，post到后台删除服务器文件等操作，
-        //传递参数：
-        //图片： imgpath --图片路径
-        //视频： filepath --视频路径 imgpath --封面路径
-        //附件： filepath --附件路径
-        //,
-        calldel: {
-            headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-            url: url + 'upload/deletefile',
-            datatype: 'json',
-            async: 'true',
-        }
-        //测试参数
-        ,
-        backDelImg: true
-        //开发者模式 --默认为false
-        ,
-        devmode: false
-        //是否自动同步到textarea
-        ,
-        autoSync: true
-        //内容改变监听事件
-        ,
-        onchange: function (content) {
-            console.log(content);
-        }
-        //插入代码设置 --hide:false 等同于不配置codeConfig
-        ,
-        codeConfig: {
-            hide: true, //是否隐藏编码语言选择框
-            default: 'javascript', //hide为true时的默认语言格式
-            encode: true //是否转义
-            ,
-            class: 'layui-code' //默认样式
-        },
-        tool: [
-            'code', 'strong', 'italic', 'underline', 'del', 'addhr', '|', 'removeformat', 'fontFomatt', 'fontfamily', 'fontSize', 'fontBackColor', 'colorpicker', 'face', '|', 'left', 'center', 'right', '|', 'link', 'unlink', 'image_alt', '|', 'table', 'fullScreen'
-        ],
-        height: '500px'
-    });
-    textcontent = layedit.build('L_content');
+
     form.verify({
         articletype: function (value) {
             if (value == '...') {
@@ -100,7 +21,7 @@ layui.use(['form', 'layer', 'layedit'], function () {
             }
         },
         content:function () {
-            var content=layedit.getContent(textcontent);
+            var content=editor.document.getBody().getText();
             if (content=="") {
                 return '请填写正文内容';
             }
@@ -119,14 +40,14 @@ layui.use(['form', 'layer', 'layedit'], function () {
                 btn: ['确定'],
                 btnAlign: 'c',
                 moveType: 1, //拖拽模式，0或者1
-                content: layedit.getContent(textcontent)
+                content: editor.document.getBody().getHtml()
             });
         },
         save: function () {
             form.on('submit(save)', function (data) {
                 var articleData = data.field;
-                var content = layedit.getContent(textcontent);
-                var textsection = layedit.getText(textcontent);
+                var content = editor.document.getBody().getHtml();
+                var textsection = editor.document.getBody().getText();
                 if (textsection.length > 170)
                     textsection = textsection.substring(-1, 170);
                 loading = layer.load(2);
@@ -165,9 +86,9 @@ layui.use(['form', 'layer', 'layedit'], function () {
         publish: function () {
             form.on('submit(publish)', function (data) {
                 var articleData = data.field;
-                var content = layedit.getContent(textcontent);
-                var textsection = layedit.getText(textcontent);
-                if (textsection.length > 200)
+                var content = editor.document.getBody().getHtml();
+                var textsection = editor.document.getBody().getText();
+                if (textsection.length > 170)
                     textsection = textsection.substring(-1, 170);
                 loading = layer.load(2);
                 var model={
@@ -243,7 +164,8 @@ function bindArticle(id){
                     "title": response.data.title,
                     "content": response.data.content
                 });
-                layedit.setContent(textcontent, response.data.content);
+                editor.setData(response.data.content);
+                //layedit.setContent(textcontent, response.data.content);
                 layer.close(loading);
             }
         },                   
